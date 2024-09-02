0a84b587a796c4052354256b8ae0fbaefc0d05db
yndu13 <yndu13@gmail.com>
2024-09-02 17:08:45 +0800

    feat: support CLI profile credentials provider

--- src/main/java/com/aliyun/credentials/provider/DefaultCredentialsProvider.java
+++ src/main/java/com/aliyun/credentials/provider/DefaultCredentialsProvider.java
@@ -2,16 +2,23 @@ package com.aliyun.credentials.provider;
 
 import com.aliyun.credentials.exception.CredentialException;
 import com.aliyun.credentials.models.CredentialModel;
+import com.aliyun.credentials.utils.AuthConstant;
 import com.aliyun.credentials.utils.AuthUtils;
+import com.aliyun.credentials.utils.StringUtils;
+import com.aliyun.tea.utils.Validate;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 public class DefaultCredentialsProvider implements AlibabaCloudCredentialsProvider {
-    private List<AlibabaCloudCredentialsProvider> defaultProviders = new ArrayList<AlibabaCloudCredentialsProvider>();
-    private static final List<AlibabaCloudCredentialsProvider> USER_CONFIGURATION_PROVIDERS =
-            new Vector<AlibabaCloudCredentialsProvider>();
+    private final List<AlibabaCloudCredentialsProvider> defaultProviders = new ArrayList<AlibabaCloudCredentialsProvider>();
+    private static final List<AlibabaCloudCredentialsProvider> USER_CONFIGURATION_PROVIDERS = new Vector<AlibabaCloudCredentialsProvider>();
 
     public DefaultCredentialsProvider() {
         defaultProviders.add(new SystemPropertiesCredentialsProvider());
@@ -23,6 +30,7 @@ public class DefaultCredentialsProvider implements AlibabaCloudCredentialsProvid
                     .oidcTokenFilePath(AuthUtils.getEnvironmentOIDCTokenFilePath())
                     .build());
         }
+        defaultProviders.add(CLIProfileCredentialsProvider.builder().build());
         defaultProviders.add(new ProfileCredentialsProvider());
         String roleName = AuthUtils.getEnvironmentECSMetaData();
         if (null != roleName) {
@@ -66,5 +74,249 @@ public class DefaultCredentialsProvider implements AlibabaCloudCredentialsProvid
 
     public static void clearCredentialsProvider() {
         DefaultCredentialsProvider.USER_CONFIGURATION_PROVIDERS.clear();
+    }
+
+}
+
+/**
+ * CLIProfileCredentialsProvider is not public.
+ */
+class CLIProfileCredentialsProvider implements AlibabaCloudCredentialsProvider {
+    private final String CLI_CREDENTIALS_CONFIG_PATH = System.getProperty("user.home") +
+            "/.aliyun/config.json";
+    private volatile AlibabaCloudCredentialsProvider credentialsProvider;
+    private volatile String currentProfileName;
+    private final Object credentialsProviderLock = new Object();
+
+    private CLIProfileCredentialsProvider(Builder builder) {
+        this.currentProfileName = builder.profileName;
+    }
+
+    static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public CredentialModel getCredentials() {
+        try {
+            return refreshCredentials();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    CredentialModel refreshCredentials() {
+        if (AuthUtils.isDisableCLIProfile()) {
+            throw new CredentialException("CLI credentials file is disabled.");
+        }
+        Config config = parseProfile(CLI_CREDENTIALS_CONFIG_PATH);
+        if (null == config) {
+            throw new CredentialException("Unable to get profile form empty CLI credentials file.");
+        }
+        String refreshedProfileName = System.getenv("ALIBABA_CLOUD_PROFILE");
+        if (shouldReloadCredentialsProvider(refreshedProfileName)) {
+            synchronized (credentialsProviderLock) {
+                if (shouldReloadCredentialsProvider(refreshedProfileName)) {
+                    if (!StringUtils.isEmpty(refreshedProfileName)) {
+                        this.currentProfileName = refreshedProfileName;
+                    }
+                    this.credentialsProvider = reloadCredentialsProvider(config, this.currentProfileName);
+                }
+            }
+        }
+        return this.credentialsProvider.getCredentials();
+    }
+
+    AlibabaCloudCredentialsProvider reloadCredentialsProvider(Config config, String profileName) {
+        String currentProfileName = !StringUtils.isEmpty(profileName) ? profileName : config.getCurrent();
+        List<Profile> profiles = config.getProfiles();
+        if (profiles != null && !profiles.isEmpty()) {
+            for (Profile profile : profiles) {
+                if (!StringUtils.isEmpty(profile.getName()) && profile.getName().equals(currentProfileName)) {
+                    switch (profile.getMode()) {
+                        case "AK":
+                            return StaticCredentialsProvider.builder()
+                                    .credential(CredentialModel.builder()
+                                            .accessKeyId(Validate.notNull(
+                                                    profile.getAccessKeyId(), "AccessKeyId must not be null."))
+                                            .accessKeySecret(Validate.notNull(
+                                                    profile.getAccessKeySecret(), "AccessKeySecret must not be null."))
+                                            .type(AuthConstant.ACCESS_KEY)
+                                            .build())
+                                    .build();
+                        case "RamRoleArn":
+                            AlibabaCloudCredentialsProvider innerProvider = StaticCredentialsProvider.builder()
+                                    .credential(CredentialModel.builder()
+                                            .accessKeyId(Validate.notNull(
+                                                    profile.getAccessKeyId(), "AccessKeyId must not be null."))
+                                            .accessKeySecret(Validate.notNull(
+                                                    profile.getAccessKeySecret(), "AccessKeySecret must not be null."))
+                                            .type(AuthConstant.ACCESS_KEY)
+                                            .build())
+                                    .build();
+                            ;
+                            return RamRoleArnCredentialProvider.builder()
+                                    .credentialsProvider(innerProvider)
+                                    .durationSeconds(profile.getDurationSeconds() != null ? profile.getDurationSeconds() : 3600)
+                                    .roleArn(profile.getRoleArn())
+                                    .roleSessionName(profile.getRoleSessionName())
+                                    .build();
+                        case "EcsRamRole":
+                            return EcsRamRoleCredentialProvider.builder()
+                                    .roleName(profile.getRamRoleName())
+                                    .build();
+                        case "OIDC":
+                            return OIDCRoleArnCredentialProvider.builder()
+                                    .durationSeconds(profile.getDurationSeconds() != null ? profile.getDurationSeconds() : 3600)
+                                    .roleArn(profile.getRoleArn())
+                                    .roleSessionName(profile.getRoleSessionName())
+                                    .oidcProviderArn(profile.getOidcProviderArn())
+                                    .oidcTokenFilePath(profile.getOidcTokenFile())
+                                    .build();
+                        case "ChainableRamRoleArn":
+                            AlibabaCloudCredentialsProvider previousProvider = reloadCredentialsProvider(config, profile.getSourceProfile());
+                            return RamRoleArnCredentialProvider.builder()
+                                    .credentialsProvider(previousProvider)
+                                    .durationSeconds(profile.getDurationSeconds() != null ? profile.getDurationSeconds() : 3600)
+                                    .roleArn(profile.getRoleArn())
+                                    .roleSessionName(profile.getRoleSessionName())
+                                    .build();
+                        default:
+                            throw new CredentialException(String.format("Unsupported profile mode '%s' form CLI credentials file.", profile.getMode()));
+                    }
+                }
+            }
+        }
+        throw new CredentialException(String.format("Unable to get profile with '%s' form CLI credentials file.", currentProfileName));
+    }
+
+    Config parseProfile(String configFilePath) {
+        File configFile = new File(configFilePath);
+        if (!configFile.exists() || !configFile.isFile() || !configFile.canRead()) {
+            throw new CredentialException(String.format("Unable to open credentials file: %s.", configFile.getAbsolutePath()));
+        }
+        Gson gson = new Gson();
+        try (BufferedReader br = new BufferedReader(new FileReader(configFile))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            String jsonContent = sb.toString();
+            return gson.fromJson(jsonContent, Config.class);
+        } catch (Exception e) {
+            throw new CredentialException(String.format("Failed to parse credential form CLI credentials file: %s.", configFile.getAbsolutePath()));
+        }
+    }
+
+    boolean shouldReloadCredentialsProvider(String profileName) {
+        return this.credentialsProvider == null || (!StringUtils.isEmpty(this.currentProfileName) && !StringUtils.isEmpty(profileName) && !this.currentProfileName.equals(profileName));
+    }
+
+    String getProfileName() {
+        return this.currentProfileName;
+    }
+
+    static final class Builder {
+        private String profileName = System.getenv("ALIBABA_CLOUD_PROFILE");
+
+        public Builder profileName(String profileName) {
+            this.profileName = profileName;
+            return this;
+        }
+
+        CLIProfileCredentialsProvider build() {
+            return new CLIProfileCredentialsProvider(this);
+        }
+    }
+
+    static class Config {
+        @SerializedName("current")
+        private String current;
+        @SerializedName("profiles")
+        private List<Profile> profiles;
+
+        public String getCurrent() {
+            return current;
+        }
+
+        public List<Profile> getProfiles() {
+            return profiles;
+        }
+    }
+
+    static class Profile {
+        @SerializedName("name")
+        private String name;
+        @SerializedName("mode")
+        private String mode;
+        @SerializedName("access_key_id")
+        private String accessKeyId;
+        @SerializedName("access_key_secret")
+        private String accessKeySecret;
+        @SerializedName("ram_role_arn")
+        private String roleArn;
+        @SerializedName("ram_session_name")
+        private String roleSessionName;
+        @SerializedName("expired_seconds")
+        private Integer durationSeconds;
+        @SerializedName("sts_region")
+        private String stsRegionId;
+        @SerializedName("ram_role_name")
+        private String ramRoleName;
+        @SerializedName("oidc_token_file")
+        private String oidcTokenFile;
+        @SerializedName("oidc_provider_arn")
+        private String oidcProviderArn;
+        @SerializedName("source_profile")
+        private String sourceProfile;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getMode() {
+            return mode;
+        }
+
+        public String getAccessKeyId() {
+            return accessKeyId;
+        }
+
+        public String getAccessKeySecret() {
+            return accessKeySecret;
+        }
+
+        public String getRoleArn() {
+            return roleArn;
+        }
+
+        public String getRoleSessionName() {
+            return roleSessionName;
+        }
+
+        public Integer getDurationSeconds() {
+            return durationSeconds;
+        }
+
+        public String getStsRegionId() {
+            return stsRegionId;
+        }
+
+        public String getRamRoleName() {
+            return ramRoleName;
+        }
+
+        public String getOidcTokenFile() {
+            return oidcTokenFile;
+        }
+
+        public String getOidcProviderArn() {
+            return oidcProviderArn;
+        }
+
+        public String getSourceProfile() {
+            return sourceProfile;
+        }
     }
 }
---
+++ src/test/java/com/aliyun/credentials/provider/CLIProfileCredentialsProviderTest.java
@@ -0,0 +1,166 @@
+package com.aliyun.credentials.provider;
+
+import com.aliyun.credentials.exception.CredentialException;
+import com.aliyun.credentials.models.CredentialModel;
+import com.aliyun.credentials.utils.AuthUtils;
+import com.google.gson.Gson;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CLIProfileCredentialsProviderTest {
+
+    @Test
+    public void getProfileNameTest() {
+        CLIProfileCredentialsProvider provider = CLIProfileCredentialsProvider.builder().build();
+        Assert.assertNull(provider.getProfileName());
+        provider = CLIProfileCredentialsProvider.builder()
+                .profileName("AK")
+                .build();
+        Assert.assertEquals("AK", provider.getProfileName());
+    }
+
+    @Test
+    public void shouldReloadCredentialsProviderTest() {
+        CLIProfileCredentialsProvider provider = CLIProfileCredentialsProvider.builder().build();
+        Assert.assertTrue(provider.shouldReloadCredentialsProvider(""));
+    }
+
+    @Test
+    public void disableCLIProfileTest() {
+        boolean isDisableCLIProfile = AuthUtils.isDisableCLIProfile();
+        AuthUtils.disableCLIProfile(true);
+        CLIProfileCredentialsProvider provider = CLIProfileCredentialsProvider.builder().build();
+        try {
+            provider.refreshCredentials();
+            Assert.fail();
+        } catch (CredentialException e) {
+            Assert.assertEquals("CLI credentials file is disabled.", e.getMessage());
+        }
+        AuthUtils.disableCLIProfile(isDisableCLIProfile);
+    }
+
+    @Test
+    public void parseProfileTest() {
+        CLIProfileCredentialsProvider provider = CLIProfileCredentialsProvider.builder().build();
+        try {
+            provider.parseProfile("./not_exist_config.json");
+            Assert.fail();
+        } catch (CredentialException e) {
+            Assert.assertTrue(e.getMessage().contains("Unable to open credentials file"));
+        }
+        try {
+            String configPath = CLIProfileCredentialsProviderTest.class.getClassLoader().
+                    getResource("invalid_cli_config.json").getPath();
+            provider.parseProfile(configPath);
+            Assert.fail();
+        } catch (CredentialException e) {
+            Assert.assertTrue(e.getMessage().contains("Failed to parse credential form CLI credentials file"));
+        }
+
+        String configPath = CLIProfileCredentialsProviderTest.class.getClassLoader().
+                getResource("empty_file.json").getPath();
+        CLIProfileCredentialsProvider.Config config = provider.parseProfile(configPath);
+        Assert.assertNull(config);
+
+        configPath = CLIProfileCredentialsProviderTest.class.getClassLoader().
+                getResource("mock_empty_cli_config.json").getPath();
+        config = provider.parseProfile(configPath);
+        Assert.assertNull(config.getCurrent());
+        Assert.assertNull(config.getProfiles());
+
+        configPath = CLIProfileCredentialsProviderTest.class.getClassLoader().
+                getResource("full_cli_config.json").getPath();
+        config = provider.parseProfile(configPath);
+        Assert.assertEquals("AK", config.getCurrent());
+        Assert.assertEquals(5, config.getProfiles().size());
+        Assert.assertEquals("[{\"name\":\"AK\",\"mode\":\"AK\",\"access_key_id\":\"access_key_id\",\"access_key_secret\":\"access_key_secret\"},{\"name\":\"RamRoleArn\",\"mode\":\"RamRoleArn\",\"access_key_id\":\"access_key_id\",\"access_key_secret\":\"access_key_secret\",\"ram_role_arn\":\"ram_role_arn\",\"ram_session_name\":\"ram_session_name\",\"expired_seconds\":3600,\"sts_region\":\"cn-hangzhou\"},{\"name\":\"EcsRamRole\",\"mode\":\"EcsRamRole\",\"ram_role_name\":\"ram_role_name\"},{\"name\":\"OIDC\",\"mode\":\"OIDC\",\"ram_role_arn\":\"ram_role_arn\",\"ram_session_name\":\"ram_session_name\",\"expired_seconds\":3600,\"sts_region\":\"cn-hangzhou\",\"oidc_token_file\":\"path/to/oidc/file\",\"oidc_provider_arn\":\"oidc_provider_arn\"},{\"name\":\"ChainableRamRoleArn\",\"mode\":\"ChainableRamRoleArn\",\"ram_role_arn\":\"ram_role_arn\",\"ram_session_name\":\"ram_session_name\",\"expired_seconds\":3600,\"sts_region\":\"cn-hangzhou\",\"source_profile\":\"AK\"}]", new Gson().toJson(config.getProfiles()));
+    }
+
+    @Test
+    public void reloadCredentialsProviderTest() {
+        CLIProfileCredentialsProvider provider = CLIProfileCredentialsProvider.builder().build();
+        String configPath = CLIProfileCredentialsProviderTest.class.getClassLoader().
+                getResource(".aliyun/config.json").getPath();
+        CLIProfileCredentialsProvider.Config config = provider.parseProfile(configPath);
+        try {
+            provider.reloadCredentialsProvider(config, "inexist");
+            Assert.fail();
+        } catch (CredentialException e) {
+            Assert.assertEquals("Unable to get profile with 'inexist' form CLI credentials file.", e.getMessage());
+        }
+
+        AlibabaCloudCredentialsProvider credentialsProvider = provider.reloadCredentialsProvider(config, "AK");
+        Assert.assertTrue(credentialsProvider instanceof StaticCredentialsProvider);
+        CredentialModel credential = credentialsProvider.getCredentials();
+        Assert.assertEquals("akid", credential.getAccessKeyId());
+        Assert.assertEquals("secret", credential.getAccessKeySecret());
+        Assert.assertNull(credential.getSecurityToken());
+
+        credentialsProvider = provider.reloadCredentialsProvider(config, "RamRoleArn");
+        Assert.assertTrue(credentialsProvider instanceof RamRoleArnCredentialProvider);
+        credential = credentialsProvider.getCredentials();
+        Assert.assertNull(credential);
+
+        try {
+            provider.reloadCredentialsProvider(config, "Invalid_RamRoleArn");
+            Assert.fail();
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("AccessKeyId must not be null.", e.getMessage());
+        }
+
+        credentialsProvider = provider.reloadCredentialsProvider(config, "EcsRamRole");
+        Assert.assertTrue(credentialsProvider instanceof EcsRamRoleCredentialProvider);
+
+        credentialsProvider = provider.reloadCredentialsProvider(config, "OIDC");
+        Assert.assertTrue(credentialsProvider instanceof OIDCRoleArnCredentialProvider);
+
+        credentialsProvider = provider.reloadCredentialsProvider(config, "ChainableRamRoleArn");
+        Assert.assertTrue(credentialsProvider instanceof RamRoleArnCredentialProvider);
+        Assert.assertEquals("akid", ((RamRoleArnCredentialProvider) credentialsProvider).getAccessKeyId());
+        Assert.assertEquals("secret", ((RamRoleArnCredentialProvider) credentialsProvider).getAccessKeySecret());
+
+        try {
+            provider.reloadCredentialsProvider(config, "ChainableRamRoleArn2");
+            Assert.fail();
+        } catch (CredentialException e) {
+            Assert.assertEquals("Unable to get profile with 'InvalidSource' form CLI credentials file.", e.getMessage());
+        }
+
+        try {
+            provider.reloadCredentialsProvider(config, "Unsupported");
+            Assert.fail();
+        } catch (CredentialException e) {
+            Assert.assertEquals("Unsupported profile mode 'Unsupported' form CLI credentials file.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void refreshCredentialsTest() {
+        String homePath = System.getProperty("user.home");
+        String configPath = CLIProfileCredentialsProviderTest.class.getClassLoader().
+                getResource(".aliyun/config.json").getPath();
+        System.setProperty("user.home", configPath.replace("/.aliyun/config.json", ""));
+        CLIProfileCredentialsProvider provider = CLIProfileCredentialsProvider.builder().build();
+        CredentialModel credential = provider.refreshCredentials();
+        Assert.assertEquals("akid", credential.getAccessKeyId());
+        Assert.assertEquals("secret", credential.getAccessKeySecret());
+        Assert.assertNull(credential.getSecurityToken());
+
+        provider = CLIProfileCredentialsProvider.builder().profileName("inexist").build();
+        try {
+            provider.refreshCredentials();
+            Assert.fail();
+        } catch (CredentialException e) {
+            Assert.assertEquals("Unable to get profile with 'inexist' form CLI credentials file.", e.getMessage());
+        }
+
+        Assert.assertNull(provider.getCredentials());
+
+        provider = CLIProfileCredentialsProvider.builder().profileName("RamRoleArn").build();
+        credential = provider.refreshCredentials();
+        Assert.assertNull(credential);
+
+        System.setProperty("user.home", homePath);
+    }
+
+}
